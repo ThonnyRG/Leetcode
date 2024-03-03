7a1f95e6d6d52696bf98ce44074c1a12f89c6f26
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-03-02 18:17:45 -0600

    Daily, 2 March 2024

---
+++ Leetcode19.dart
@@ -0,0 +1,32 @@
+class Solution {
+  ListNode? reverseList(ListNode? head) {
+    ListNode? prev = null, curr = head, temp;
+    while (curr != null) {
+      temp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = temp;
+    }
+    return prev;
+  }
+
+  ListNode? removeNthFromEnd(ListNode? head, int n) {
+    if (head!.next == null) return null;
+    ListNode? newHead = reverseList(head), prev, curr = newHead;
+    while (--n > 0) {
+      prev = curr;
+      curr = curr!.next;
+    }
+    if (prev == null) {
+      prev = curr;
+      curr = curr!.next;
+      prev.next = null;
+      head = reverseList(curr);
+    } else {
+      prev.next = curr!.next;
+      curr.next = null;
+      head = reverseList(newHead);
+    }
+    return head;
+  }
+}
