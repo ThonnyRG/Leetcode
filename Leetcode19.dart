c30ac3ead8a6a33c6c3569ba776b0003c2082e3d
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-03-02 18:18:22 -0600

    version 2, Daily 2 march 2024

--- Leetcode19.dart
+++ Leetcode19.dart
@@ -1,6 +1,6 @@
 class Solution {
   ListNode? reverseList(ListNode? head) {
-    ListNode? prev = null, curr = head, temp;
+    ListNode? prev = null, curr = head, temp = null;
     while (curr != null) {
       temp = curr.next;
       curr.next = prev;
@@ -11,20 +11,21 @@ class Solution {
   }
 
   ListNode? removeNthFromEnd(ListNode? head, int n) {
-    if (head!.next == null) return null;
-    ListNode? newHead = reverseList(head), prev, curr = newHead;
+    if (head?.next == null) return null;
+    ListNode? newHead = reverseList(head), prev = null, curr = newHead;
     while (--n > 0) {
       prev = curr;
-      curr = curr!.next;
+      curr = curr?.next;
     }
     if (prev == null) {
+      // the problem is asking to delete the last node itself
       prev = curr;
-      curr = curr!.next;
-      prev.next = null;
+      curr = curr?.next;
+      prev?.next = null;
       head = reverseList(curr);
     } else {
-      prev.next = curr!.next;
-      curr.next = null;
+      prev.next = curr?.next;
+      curr?.next = null;
       head = reverseList(newHead);
     }
     return head;
