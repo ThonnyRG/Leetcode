505ab931fb71926439aa566c84d293cef6c0f569
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-03-11 18:31:16 -0600

    Daly, second amptemp

---
+++ leetcode1171.java
@@ -0,0 +1,23 @@
+public ListNode removeZeroSumSublists(ListNode head) {
+    ListNode dummy = new ListNode(0), cur = dummy;
+    dummy.next = head;
+    int prefix = 0;
+    Map<Integer, ListNode> m = new HashMap<>();
+    while (cur != null) {
+        prefix += cur.val;
+        if (m.containsKey(prefix)) {
+            cur =  m.get(prefix).next;
+            int p = prefix + cur.val;
+            while (p != prefix) {
+                m.remove(p);
+                cur = cur.next;
+                p += cur.val;
+            }
+            m.get(prefix).next = cur.next;
+        } else {
+            m.put(prefix, cur);
+        }
+        cur = cur.next;
+    }
+    return dummy.next;
+}
