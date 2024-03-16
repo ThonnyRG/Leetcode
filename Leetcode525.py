0f84ba2d1e4271d83de583d12b4cfdb718533fe9
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-03-15 18:41:13 -0600

    Daily 15 march 2024

---
+++ Leetcode525.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        max_length = 0
+        sum_map = {0: -1}
+        count = 0
+        
+        for i, num in enumerate(nums):
+            if num == 0:
+                count -= 1
+            else:
+                count += 1
+                
+            if count in sum_map:
+                max_length = max(max_length, i - sum_map[count])
+            else:
+                sum_map[count] = i
+        
+        return max_length
