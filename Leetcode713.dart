ed1cc2e19e3ec8f160c93feaa8058e75ded0d9ac
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-03-26 18:53:44 -0600

    Daily 26 march 2024

---
+++ Leetcode713.dart
@@ -0,0 +1,36 @@
+/*
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k==0) return 0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            long product=1;
+            for(int j=i;j<nums.length;j++){
+                product*=nums[j];
+                if(product<k) count++;
+                else break;
+            }
+        }
+        return count;
+    }
+}
+
+*/
+
+class Solution {
+  int numSubarrayProductLessThanK(List<int> nums, int k) {
+    if(k == 0)
+    return 0;
+    int count = 0;
+    for (int i = 0; i < nums.length; i++) {
+      int product = 1;
+      for (int j = i; j < nums.length; j++) {
+        product *= nums[j];
+        if(product < k) count++; 
+        else break;
+      }
+    }
+    return count;
+
+  }
+}
