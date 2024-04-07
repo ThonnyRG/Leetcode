4a8e9f122ffad56b55c0968fb3f68a8a4be43012
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-04-06 18:39:53 -0600

    daily 6 marhc 2024, done

--- Leetcode678.dart
+++ Leetcode678.dart
@@ -1,5 +1,22 @@
 class Solution {
   bool checkValidString(String s) {
-    
+    int leftMin = 0, leftMax = 0;
+
+    for (var c in s.runes) {
+      if (String.fromCharCode(c) == '(') {
+        leftMin++;
+        leftMax++;
+      } else if (String.fromCharCode(c) == ')') {
+        leftMin--;
+        leftMax--;
+      } else {
+        leftMin--;
+        leftMax++;
+      }
+      if (leftMax < 0) return false;
+      if (leftMin < 0) leftMin = 0;
+    }
+
+    return leftMin == 0;
   }
 }
