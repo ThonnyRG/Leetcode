7c70b9e300c4b5194f4ccbbe3861a85c66923ad3
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-04-16 18:38:39 -0600

    Daily 16 apr 2024

---
+++ Leetcode988.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String smallestFromLeaf(TreeNode root) {
+        recur(root, new StringBuilder());
+        return ans;
+    }
+    private String ans = "~";
+    private void recur(TreeNode node, StringBuilder holder) {
+
+        if (node != null) {
+            holder.append((char)('a' + node.val));
+
+            if (node.left == null && node.right == null) {
+                holder.reverse();
+                String str = holder.toString();
+                holder.reverse();
+
+                if (str.compareTo(ans) < 0) {
+                    ans = str;
+                }
+            }
+            recur(node.left, holder);
+            recur(node.right, holder);
+            holder.deleteCharAt(holder.length() - 1);
+        }
+    }
+}
