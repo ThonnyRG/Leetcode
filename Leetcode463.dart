44ad49a6ff8e1616c279c0a05e655442382f14d2
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-04-17 18:55:23 -0600

    Daily 17 apr 2024

---
+++ Leetcode463.dart
@@ -0,0 +1,26 @@
+class Solution {
+  int islandPerimeter(List<List<int>> grid) {
+    int ans = 0;
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] == 1) {
+          int count = 0;
+          if (i - 1 >= 0 && grid[i - 1][j] == 1) {
+            count++;
+          }
+          if (j - 1 >= 0 && grid[i][j - 1] == 1) {
+            count++;
+          }
+          if (i + 1 < grid.length && grid[i + 1][j] == 1) {
+            count++;
+          }
+          if (j + 1 < grid[0].length && grid[i][j + 1] == 1) {
+            count++;
+          }
+          ans += 4 - count;
+        }
+      }
+    }
+    return ans;
+  }
+}
