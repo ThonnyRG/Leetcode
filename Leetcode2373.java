75569b1b4aec50845b0ffbe00bbfc825314c056d
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-11 18:44:24 -0600

    Daily 11 May 2024

--- Leetcode2373.java
+++ Leetcode2373.java
@@ -1,5 +1,22 @@
 class Solution {
     public int[][] largestLocal(int[][] grid) {
-        
+        int n = grid.length, m = grid[0].length;
+        int[][] res = new int[n - 2][m - 2];
+
+        for(int i = 1; i < n - 1; ++i) {
+            for(int j = 1; j < m - 1; ++j) {
+                int temp = 0;
+
+                for(int k = i - 1; k <= i + 1; ++k) {
+                    for(int l = j - 1; l <= j + 1; ++l) {
+                        temp = Math.max(temp, grid[k][l]);
+                    }
+                }
+
+                res[i - 1][j - 1] = temp;
+            }
+        }
+
+        return res;
     }
 }
