caf06bc62e5132a42174cad25ef3217f18a9768e
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-17 18:26:39 -0600

    Daily 17 May 2024

--- Leetcodes/Leetcode979.java
+++ Leetcodes/Leetcode979.java
@@ -14,7 +14,25 @@
  * }
  */
 class Solution {
+    int moves;
     public int distributeCoins(TreeNode root) {
         
+        moves = 0;
+        dfs(root);
+        return moves;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        int leftBalance = dfs(node.left);
+        int rightBalance = dfs(node.right);
+        
+        int balance = node.val + leftBalance + rightBalance - 1;
+        moves += Math.abs(balance);
+        
+        return balance;
     }
 }
