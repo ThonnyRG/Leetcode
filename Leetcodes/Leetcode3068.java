cefc1d11f81052377e3531ddfc404baae2145127
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-18 18:55:40 -0600

    Daily 18 May 2024

--- Leetcodes/Leetcode3068.java
+++ Leetcodes/Leetcode3068.java
@@ -1,5 +1,18 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        return 0;
+        int n = nums.length;
+        long[] getXORDiff = new long[n];
+        for (int i = 0; i < n; ++i)
+            getXORDiff[i] = (nums[i]^k)-nums[i];
+        Arrays.sort(getXORDiff);
+        long maxSum = 0;
+        for (int x : nums)
+            maxSum+=x;
+        for (int i = n-1; i >= 1; i-=2) {
+            if (getXORDiff[i]+getXORDiff[i-1]<0)
+                return maxSum;
+            maxSum += getXORDiff[i]+getXORDiff[i-1];
+        }
+        return maxSum;
     }
 }
