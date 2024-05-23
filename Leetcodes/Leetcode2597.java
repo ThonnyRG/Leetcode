88bf23dd9a941ebc40616957fc7ecaa19aeae5bd
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-22 18:51:26 -0600

    Daily 22 May 2024

--- Leetcodes/Leetcode2597.java
+++ Leetcodes/Leetcode2597.java
@@ -1,5 +1,22 @@
 class Solution {
-    public int beautifulSubsets(int[] nums, int k) {
-        
+    private int dfs(int[] nums, int k, int i, Set<Integer> set) {
+      if (i == nums.length)
+       return set.isEmpty() ? 0 : 1;
+  
+      var cnt = dfs(nums, k, i+1, set);
+      
+      if (set.contains(nums[i] - k)) return cnt;
+  
+      set.add(nums[i]);
+      cnt += dfs(nums, k, i+1 ,set);
+      set.remove(nums[i]);
+  
+      return cnt;
     }
-}
+  
+    public int beautifulSubsets(int[] nums, int k) {
+      Arrays.sort(nums);
+  
+      return dfs(nums, k, 0, new HashSet<Integer>());
+    }
+  }
