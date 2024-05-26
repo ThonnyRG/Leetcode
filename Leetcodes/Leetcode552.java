0d9f8def907b9d6e3fd3a23d5427a96fbe0ce03e
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-25 18:38:13 -0600

    Daily 25 May 2024

--- Leetcodes/Leetcode552.java
+++ Leetcodes/Leetcode552.java
@@ -1,5 +1,28 @@
 class Solution {
+    private int mod= 1000000007;
+    int dp[][][] = new int[100002][2][3];
+    public int solve(int ind,int n,int cnt_abs,int cnt_late)
+    {
+        if(ind >= n)
+        return 1;
+        if(dp[ind][cnt_abs][cnt_late]!=-1)
+        return dp[ind][cnt_abs][cnt_late];
+            int abs= cnt_abs==0?solve(ind+1,n,1,0):0;
+            int pre = solve(ind+1,n,cnt_abs,0);
+            int late=0;
+            if(cnt_late!=2)
+            late=solve(ind+1,n,cnt_abs,cnt_late+1);
+            return dp[ind][cnt_abs][cnt_late]=((pre+late)%mod+abs)%mod;
+    }
     public int checkRecord(int n) {
+         for (int i = 0; i < 100002; i++) {
+             for (int j = 0; j < 2; j++) {
+                 for (int k = 0; k < 3; k++) {
+                     dp[i][j][k] = -1;
+        }
+    }
+}
+              return solve(0,n,0,0);
         
     }
 }
