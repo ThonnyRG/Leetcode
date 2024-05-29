87ecfdfe55e6c889a1676fb9286d6ee53faa5445
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-05-28 18:59:56 -0600

    Daily 28 May 2024

--- Leetcodes/Leetcode1404.java
+++ Leetcodes/Leetcode1404.java
@@ -1,5 +1,21 @@
 class Solution {
     public int numSteps(String s) {
-        
+        int x = 0;
+        int y = 0;
+
+        for(int i = s.length()-1; i >= 1; i--){
+            int num = s.charAt(i) -'0';
+            if(num == 0 && y == 0){
+                x++;
+            }else if(num == 1 && y == 1){
+                x++;
+                y = 1;
+            }else{
+                x += 2;
+                y = 1;
+            }
+        }
+        if(y == 1)x++;
+        return x;
     }
 }
