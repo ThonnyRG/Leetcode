b6ac7a2b1795bdd85c68747d6d238ac0c0a27be0
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-06-08 19:14:37 -0600

    Dialy 8 jun 2024

--- Leetcodes/Leetcode974.java
+++ Leetcodes/Leetcode974.java
@@ -1,5 +1,19 @@
-class Solution {
+public class Solution {
     public int subarraysDivByK(int[] nums, int k) {
+        int count = 0;
         
+        // Iterate through all possible subarrays
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                // Check if the sum is divisible by k
+                if (sum % k == 0) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
     }
 }
