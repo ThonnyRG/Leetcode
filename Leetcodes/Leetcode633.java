6c794a9ec171aae03ca52bcc660d5efae2f19ca8
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-06-16 19:02:46 -0600

    Daily 16 jun 2024

--- Leetcodes/Leetcode633.java
+++ Leetcodes/Leetcode633.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        
+        long a = 0, b = (long)Math.sqrt(c);
+        while (a <= b){
+            long sq = (a * a) + (b * b);
+            if (sq == c)
+                return true;
+            else if (sq < c)
+                a++;
+            else
+                b--;
+        }
+        return false;
     }
 }
