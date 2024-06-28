86d4dd09bc4e0013b41d266ec3a4692a0fe1bf04
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-06-27 18:18:21 -0600

    Daily 27 june 2024

--- Leetcodes/Leetcode2785.java
+++ Leetcodes/Leetcode2785.java
@@ -1,5 +1,17 @@
 class Solution {
     public long maximumImportance(int n, int[][] roads) {
-        
+      var degrees = new long[n];
+      var sum = 0L;
+  
+      for (var road : roads) {
+        degrees[road[0]]++;
+        degrees[road[1]]++;
+      }
+      Arrays.sort(degrees);
+  
+      for (var i=0; i<n; i++)
+        sum += degrees[i] * (i+1);
+  
+      return sum;
     }
-}
+  }
