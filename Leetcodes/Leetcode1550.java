859587dc03a2524080fec4feca36d87f8785fe94
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-06-30 18:04:04 -0600

    Daily 30 june 2024

--- Leetcodes/Leetcode1550.java
+++ Leetcodes/Leetcode1550.java
@@ -1,5 +1,15 @@
-class Solution {
+public class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
-        
+        int n = arr.length;
+        for (int i = 0; i <= n - 3; i++) {
+            if ((arr[i] & 1) != 0) {
+                if ((arr[i + 1] & 1) != 0) {
+                    if ((arr[i + 2] & 1) != 0)
+                        return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
