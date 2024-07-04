c425a05e7044e56b036219c8ea52cd5bcf49d667
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-03 18:18:35 -0600

    Daily 3 July 2024

--- Leetcodes/Leetcode2181.java
+++ Leetcodes/Leetcode2181.java
@@ -7,9 +7,27 @@
  *     ListNode(int val) { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
+}
  */
 class Solution {
     public ListNode mergeNodes(ListNode head) {
-        
+        ListNode l2 = head;
+        ListNode head2=l2;
+        int sum=0;
+        ListNode curr=head.next;
+        while(curr!=null){
+            if(curr.val==0){
+            l2.val=sum;
+            sum=0;
+            if(curr.next==null)
+            l2.next=null;
+            else
+            l2=l2.next;
+            }
+            if(curr.val!=0)
+            sum=sum+curr.val;
+            curr=curr.next;
+        }
+        return head2;
     }
 }
