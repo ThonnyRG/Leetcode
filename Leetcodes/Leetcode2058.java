482cb104de5959ca884a7148aa9c609c61d29e4a
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-04 18:11:06 -0600

    Class empty

--- Leetcodes/Leetcode2058.java
+++ Leetcodes/Leetcode2058.java
@@ -1,3 +1,15 @@
-public class Leetcode2058 {
-    
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        
+    }
 }
