9c88fbde0598d69bfc94c78fdd30c21c2c38f917
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-04 18:17:41 -0600

    Daily 4 July 2024

--- Leetcodes/Leetcode2058.java
+++ Leetcodes/Leetcode2058.java
@@ -9,7 +9,33 @@
  * }
  */
 class Solution {
-    public int[] nodesBetweenCriticalPoints(ListNode head) {
-        
+    public int[] nodesBetweenCriticalPoints(final ListNode head) {
+        final int[] result = new int[] { -1, -1 };
+        ListNode prev = head, curr = head.next;
+        int i = 1, prevM = 0;
+
+        while(curr.next != null) {
+            if((curr.val > prev.val && curr.val > curr.next.val) || (curr.val < prev.val && curr.val < curr.next.val)) {
+                if(prevM != 0) {
+                    final int diff = i - prevM;
+
+                    if(result[0] == -1) {
+                        result[0] = diff;
+                        result[1] = diff;
+                    } else {
+                        result[0] = Math.min(result[0], diff);
+                        result[1] += diff;
+                    }
+                }
+
+                prevM = i;
+            }
+
+            i++;
+            prev = curr;
+            curr = curr.next;
+        }
+
+        return result;
     }
 }
