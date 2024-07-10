7975f2958169f4482cce5f0a09ea6dedb413d53a
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-09 18:30:08 -0600

    Daily 9 July 2024

--- Leetcodes/Leetcode1598.java
+++ Leetcodes/Leetcode1598.java
@@ -1,5 +1,13 @@
 class Solution {
     public int minOperations(String[] logs) {
-        
+        int pivot = 0;
+        for (int i = 0; i < logs.length; i++) {
+            if(pivot > 0 && logs[i].contains("../")){
+            pivot --;
+            }else if (logs[i].contains("/") && !logs[i].contains("./")){
+            pivot ++;
+            }
+        }
+        return pivot;
     }
 }
