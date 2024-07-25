052f4bace56ffd826fc30d85f1773cc71cab3df0
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-24 18:23:02 -0600

    Daily 24 june 2024

--- Leetcodes/Leetcode912.java
+++ Leetcodes/Leetcode912.java
@@ -1,5 +1,46 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = nums[j++];
+        }
+
+        for (i = left; i <= right; i++) {
+            nums[i] = temp[i - left];
+        }
     }
 }
