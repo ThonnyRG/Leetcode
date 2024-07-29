bc2e2dff0df30497486650208a439baa14e02e2c
ThonnyRG <s21017265@estudiantes.uv.mx>
2024-07-28 18:51:34 -0600

    Dayly 28 jul 2024

--- Leetcodes/Leetcode1395.java
+++ Leetcodes/Leetcode1395.java
@@ -1,5 +1,28 @@
 class Solution {
     public int numTeams(int[] rating) {
-        
+     int res = 0, n = rating.length;
+    for (int i = 1; i < n - 1; i++) {
+        int L_inc = 0,
+        L_dec = 0,
+        R_inc = 0,
+        R_dec = 0;
+        for (int j = 0; j < i; j++) {
+            if (rating[j] < rating[i]) {
+                L_inc ++;
+            }else{
+                L_dec ++;
+            }
+        }
+        for (int k = i + 1; k < n; k++) {
+            if (rating[i] < rating[k]) {
+                R_inc ++;
+            }else{
+                R_dec ++;
+            }  
+        }
+        res += (L_inc * R_inc) + (L_dec * R_dec)
     }
+    return res;
+    }
+
 }
