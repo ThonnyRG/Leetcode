ac630d2126d581fdc474fbd4a8ec82eb54dfc29f
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-29 20:09:52 -0600

    Daily 29 jul 2024

--- Leetcodes/Leetcode1653.java
+++ Leetcodes/Leetcode1653.java
@@ -6,6 +6,14 @@ class Solution {
         int output = Integer.MAX_VALUE;
         for (int i = 0; i < letters.length; i++)
             aCount += ('b' - letters[i]);
+class Solution {
+    public int minimumDeletions(String s) {
+        char[] letters = s.toCharArray();
+        int aCount = 0;
+        int bCount = 0;
+        int output = Integer.MAX_VALUE;
+        for (int i = 0; i < letters.length; i++)
+            aCount += ('b' - letters[i]);
 
         for (int i = 0; i < s.length(); i++)
         {
@@ -14,6 +22,19 @@ class Solution {
             bCount += (letters[i] - 'a');
         }
 
+        output = Math.min(output, aCount + bCount);
+        return output;
+    }
+}
+        for (int i = 0; i < s.length(); i++)
+        {
+            output = Math.min(output, aCount + bCount);
+            aCount -= ('b' - letters[i]);
+            bCount += (letters[i] - 'a');
+        }
+
+
+        
         output = Math.min(output, aCount + bCount);
         return output;
     }
