b3a8af6a7d93356fbf1ba901862b3dac7e1238ee
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-07-31 18:51:45 -0600

    Dayli 31 jul 2024

--- Leetcodes/Leetcode2678.java
+++ Leetcodes/Leetcode2678.java
@@ -1,5 +1,12 @@
 class Solution {
     public int countSeniors(String[] details) {
-        
+        int counterMayor = 0;
+        for (String detail : details) {
+            int age = Integer.parseInt(detail.substring(11, 13));
+            if (age > 60) {
+                counterMayor++;
+            }
+        }
+        return counterMayor;
     }
 }
