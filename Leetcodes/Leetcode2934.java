cfe6d31d92d084b37d3c3fc62b9921502ef2857e
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-08-01 18:24:43 -0600

    Dayly 1 aug 2024

--- Leetcodes/Leetcode2934.java
+++ Leetcodes/Leetcode2934.java
@@ -1,5 +1,32 @@
 class Solution {
     public int minSwaps(int[] nums) {
-        
+        int ones = 0;
+        for (int num : nums) {
+            ones += num;
+        }
+
+        int n = nums.length;
+        int res = nums.length;
+        int left = 0, right = 0, cnt = 0;
+
+        // Create a sliding window of size 'ones'
+        while (right < ones) {
+            cnt += nums[right % n];
+            right++;
+        }
+
+        // Calculate initial "holes" in the window
+        res = ones - cnt;
+
+        // Move the sliding window across the array
+        while (right < n + ones) {
+            cnt += nums[right % n];
+            cnt -= nums[left % n];
+            right++;
+            left++;
+            res = Math.min(res, ones - cnt);
+        }
+
+        return res;
     }
 }
