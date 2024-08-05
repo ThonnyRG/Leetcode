a8a2bb72f48669658fcd94f6211f415f9a7606e4
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-08-04 18:41:22 -0600

    Daily 4 aug 2024

--- Leetcodes/Leetcode2053.java
+++ Leetcodes/Leetcode2053.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
         
+        for (String str : arr) {
+            frequencyMap.put(str, frequencyMap.getOrDefault(str, 0) + 1);
+        }
+        
+        List<String> distinctElements = new ArrayList<>();
+        
+        for (String str : arr) {
+            if (frequencyMap.get(str) == 1) {
+                distinctElements.add(str);
+            }
+        }
+        
+        if (distinctElements.size() >= k) {
+            return distinctElements.get(k - 1);
+        } else {
+            return "";
+        }
     }
 }
