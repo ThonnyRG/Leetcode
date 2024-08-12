ce61ea5ee2527abce182031d4b69ea403189815c
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-08-11 18:11:35 -0600

    format file

--- Leetcodes/Leetcode703.java
+++ Leetcodes/Leetcode703.java
@@ -1,3 +1,16 @@
-public class Leetcode703 {
+class KthLargest {
 
+    public KthLargest(int k, int[] nums) {
+        
+    }
+    
+    public int add(int val) {
+        
+    }
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
