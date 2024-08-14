ec2dfc6d6efbb9bdfbd555fc83b92d69bcb51040
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-08-13 18:54:35 -0600

    Daily 13 aug 2024

--- Leetcodes/Leetcode719.java
+++ Leetcodes/Leetcode719.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 class Solution {
+    public int position(int[] nums, int m) {
+        int ans = 0;
+        for (int i = 0; i < nums.length - 1; ++i) {
+            int index = Arrays.binarySearch(nums, nums[i] + m + 1);
+            if (index < 0) index = -index - 1;
+            ans += (index - 1 - i);
+        }
+        return ans;
+    }
+    
     public int smallestDistancePair(int[] nums, int k) {
-        
+        Arrays.sort(nums);
+        int l = 0, n = nums.length, r = nums[n-1] - nums[0];
+        while (r - l > 1) {
+            int m = l + (r - l) / 2;
+            if (position(nums, m) >= k) {
+                r = m;
+            } else {
+                l = m;
+            }
+        }
+        if (position(nums, l) >= k) {
+            return l;
+        } else if (position(nums, r) >= k) {
+            return r;
+        }
+        return 0;
     }
 }
