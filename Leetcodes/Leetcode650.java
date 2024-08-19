0974af32678c8dce6fbf4a634420f151139bde2d
ThonnyRG <zs21017265@estudiantes.uv.mx>
2024-08-18 18:56:29 -0600

    Daily 18 aug 2024

--- Leetcodes/Leetcode650.java
+++ Leetcodes/Leetcode650.java
@@ -1,5 +1,16 @@
 class Solution {
     public int minSteps(int n) {
-        
-    }
+      int answer = 0;
+        if (n == 0) {
+            answer = 0;
+        }else{
+            for (int i = 2; n > 1; ++i) {
+                while (n % i == 0) {
+                    answer += i;
+                    n /= i;
+                }
+            }
+        }
+        return answer;
+    } 
 }
