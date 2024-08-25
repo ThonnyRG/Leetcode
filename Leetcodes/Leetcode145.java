c06bb6cddf12920d386b90ee57f49f9445f31dea
Antonio Cruz <dansevennegocios@gmai.com>
2024-08-24 18:50:36 -0600

    Daily 24 aug 2024

--- Leetcodes/Leetcode145.java
+++ Leetcodes/Leetcode145.java
@@ -14,7 +14,18 @@
  * }
  */
 class Solution {
+    private void traverse(TreeNode node, List<Integer> result) {
+        if (node == null)
+            return;
+
+        traverse(node.left, result);
+        traverse(node.right, result);
+        result.add(node.val);
+    }
+
     public List<Integer> postorderTraversal(TreeNode root) {
-        
+        List<Integer> result = new ArrayList<>();
+        traverse(root, result);
+        return result;
     }
 }
