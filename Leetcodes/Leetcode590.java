20b6c0db970d06bad7f9dbf931e3531a1de98a23
Antonio Cruz <dansevennegocios@gmai.com>
2024-08-25 18:29:38 -0600

    Daily 15 aug 2024

--- Leetcodes/Leetcode590.java
+++ Leetcodes/Leetcode590.java
@@ -19,6 +19,21 @@ class Node {
 
 class Solution {
     public List<Integer> postorder(Node root) {
-        
+        LinkedList<Integer> ans=new LinkedList<>();
+        return helper(root,ans);  
     }
+
+    public List<Integer> helper(Node root,LinkedList ans){
+        if(root==null){
+            return ans;
+        }
+
+        for(Node i : root.children){
+            helper(i,ans);
+        }
+        
+        ans.add(root.val);
+        return ans;
+    }
+    
 }
