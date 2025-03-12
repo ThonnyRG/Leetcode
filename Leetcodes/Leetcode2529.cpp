68cbf43b05c29e5320d7f186cae4b411d33174c8
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-03-11 19:58:25 -0600

    Daily 11 mar 2025

---
+++ Leetcodes/Leetcode2529.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    public:
+        int maximumCount(std::vector<int>& nums) {
+            int n = nums.size();
+            int left = 0;
+            int right = n - 1;
+    
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > 0) {
+                    right = mid - 1; 
+                } else {
+                    left = mid + 1; 
+                }
+            }
+            int positiveCount = n - left;
+    
+            left = 0; 
+            right = n - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < 0) {
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
+            int negativeCount = right + 1;
+               return std::max(positiveCount, negativeCount);
+        }
+    };
