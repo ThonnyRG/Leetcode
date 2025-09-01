958524766682a53048268daeeebae887ce743efc
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-08-31 18:09:50 -0600

    Daily 31 aug 2025

---
+++ Leetcodes/1792.maximum-average-pass-ratio.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1792 lang=cpp
+ *
+ * [1792] Maximum Average Pass Ratio
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& c, int es) {
+
+        int n  = c.size();
+        double avg  = 0,ans = 0;
+        priority_queue<pair<double,int>> pq;
+
+        for(int i = 0;i<n;i++)
+        {
+            double diff =(double)(c[i][0]+1)/(double)(c[i][1]+1)-(double)c[i][0]/(double)c[i][1];
+            pq.push({diff,i});
+        }
+        while(es--)
+        {
+           int i = pq.top().second;
+           pq.pop();
+           c[i][0]+=1;
+           c[i][1]+=1;
+           double diff = (double)(c[i][0]+1)/(double)(c[i][1]+1)-(double)c[i][0]/(double)c[i][1];
+           pq.push({diff,i});
+        }
+
+        for(int i = 0;i<n;i++)
+        {
+            avg = (double)c[i][0]/(double)c[i][1];
+            ans+=avg;
+        }
+        return ans/(double)n;
+    }
+};
+// @lc code=end
+
