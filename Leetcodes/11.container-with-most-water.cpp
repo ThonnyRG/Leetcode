ab9b1afa5ca5dad591da9d0e6ccc33721d7fabbc
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-10-03 18:19:56 -0600

    Daily 3 oct 2025

---
+++ Leetcodes/11.container-with-most-water.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+    }
+};
+// @lc code=end
+
