549ff5556e67581d8484aef7f0456f7a5c5a3409
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-10-12 18:34:28 -0600

    Daily 11 oct 2025

---
+++ Leetcodes/2273.find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=2273 lang=cpp
+ *
+ * [2273] Find Resultant Array After Removing Anagrams
+ */
+
+// @lc code=start
+class Solution {
+public:
+    static array<int, 26> freq(string& s){
+        array<int, 26> ans{};
+        for(char c: s){
+            ans[c-'a']++;
+        }
+        return ans;
+    }
+    static vector<string> removeAnagrams(vector<string>& words) {
+        int n=words.size(), l=0, r=1;
+        vector<string> ans={words[0]};
+        array<int, 26> f0=move(freq(words[0]));
+        for( ; r<n; r++){
+            string s=words[r];
+            auto x=move(freq(s));
+            if (f0!=x){
+                ans.push_back(s);
+                l=r;
+                f0=x;
+            }
+        }
+        return ans;
+    }
+};
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+// @lc code=end
+
