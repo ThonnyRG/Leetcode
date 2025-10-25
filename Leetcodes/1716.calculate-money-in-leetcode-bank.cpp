8558b8f8a46e6dd52316ba9a47fbdca01950a74f
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-10-24 19:02:46 -0600

    Daily 24 oct 2025

---
+++ Leetcodes/1716.calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1716 lang=cpp
+ *
+ * [1716] Calculate Money in Leetcode Bank
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int totalMoney(int n)
+    {
+        int w = n / 7;
+        int money = w * 28;
+        money += (7 * w * (w - 1)) / 2;
+
+        if (n % 7 != 0)
+        {
+            int extraDays = n % 7;
+            int moneyToAdd = w + 1;
+            for (int i = 0; i < extraDays; i++)
+            {
+                money += moneyToAdd;
+                moneyToAdd += 1;
+            }
+        }
+        return money;
+    }
+};
+// @lc code=end
