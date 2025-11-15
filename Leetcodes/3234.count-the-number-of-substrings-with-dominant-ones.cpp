b1527c6fe07b2ea2c51e63ef71377b620960f043
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-11-14 18:44:55 -0600

    Daily 14 nov 2025

---
+++ Leetcodes/3234.count-the-number-of-substrings-with-dominant-ones.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=3234 lang=cpp
+ *
+ * [3234] Count the Number of Substrings With Dominant Ones
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        vector<int> zero;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '0') zero.push_back(i);
+        }
+        int ones = n - zero.size(); 
+        zero.push_back(n); 
+
+        int res = 0, sid = 0; 
+
+        for (int left = 0; left < n; left++) {
+           
+            for (int id = sid; id < zero.size() - 1; id++) {
+                int cnt0 = id - sid + 1; 
+                if (cnt0 * cnt0 > ones) break;
+                int p = zero[id], q = zero[id + 1];
+                int cnt1 = zero[id] - left - (id - sid);
+                if (cnt1 >= cnt0 * cnt0) {
+                    res += q - p;
+                } else {
+                    res += max(q - p - (cnt0 * cnt0 - cnt1), 0);
+                }
+            }
+            if (s[left] == '0') {
+                sid++;
+            } else {
+               
+                res += zero[sid] - left;
+                ones--;
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+
