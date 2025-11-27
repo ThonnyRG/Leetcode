fbd82f5e032b5537b052367291edf63c861882e8
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-11-26 18:13:07 -0600

    Daily 26 nov 2025

---
+++ Leetcodes/3381.maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=3381 lang=cpp
+ *
+ * [3381] Maximum Subarray Sum With Length Divisible by K
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
