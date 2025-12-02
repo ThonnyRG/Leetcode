8ee91c489ef1b7b940055a7da9b02c4ed0be97c0
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-12-01 18:14:08 -0600

    Daily 1 dic 2025

---
+++ Leetcodes/3623.count-number-of-trapezoids-i.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=3623 lang=cpp
+ *
+ * [3623] Count Number of Trapezoids I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        int MOD = 1000000007;
+        unordered_map<int, long long> groups;
+        for (auto& point : points)
+            groups[point[1]]++;
+        long long res = 0, total = 0;
+        for (auto& group : groups){
+            long long lines = group.second * (group.second - 1) / 2;
+            res = (res + total * lines) % MOD;
+            total = (total + lines) % MOD;
+        }
+        return (int)res;
+    }
+};
+// @lc code=end
+
