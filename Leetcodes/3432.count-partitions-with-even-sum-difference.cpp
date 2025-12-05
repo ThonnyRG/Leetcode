48290e4c45db7b141b66f748184a60b0d88f9851
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-12-04 18:34:25 -0600

    Daily 4 dic 2025

---
+++ Leetcodes/3432.count-partitions-with-even-sum-difference.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=3432 lang=cpp
+ *
+ * [3432] Count Partitions with Even Sum Difference
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int leftSum = 0;
+        int rightSum = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+
+        int partitionCount = 0;
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+
+            if ((leftSum - rightSum) % 2 == 0)
+            {
+                ++partitionCount;
+            }
+            
+        }
+        return partitionCount;
+    }
+};
+// @lc code=end
+
