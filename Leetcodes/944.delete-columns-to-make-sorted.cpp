5c90a4a27407610c32adc163cc487aa384c76db1
ThonnyRG <zs21017265@estudiantes.uv.mx>
2025-12-20 18:15:13 -0600

    Daily 19 dic 2025

---
+++ Leetcodes/944.delete-columns-to-make-sorted.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=944 lang=cpp
+ *
+ * [944] Delete Columns to Make Sorted
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int delete_count=0;
+        int row = strs.size();
+        int col = strs[0].size();
+        
+        for(int j=0; j<col; j++)
+        {
+            for(int i=0; i<row-1; i++)
+            {
+                if(strs[i][j]>strs[i+1][j])
+                {
+                    delete_count++;
+                    break;
+                }
+            }
+        }
+        return delete_count;
+    }
+};
+// @lc code=end
+
