9422701aa852988bef38983a807d2ccd31818cd7
ThonnyRG <zs21017265@estudiantes.uv.mx>
2026-01-09 19:00:02 -0600

    Daily 9 jan 2026

---
+++ Leetcodes/424.longest-repeating-character-replacement.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=424 lang=python3
+#
+# [424] Longest Repeating Character Replacement
+#
+
+# @lc code=start
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        
+# @lc code=end
+
