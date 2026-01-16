15d43e297230712e867a28c971efc813aa6bbcef
ThonnyRG <zs21017265@estudiantes.uv.mx>
2026-01-15 18:48:46 -0600

    Daily 15 jan 2026

---
+++ Leetcodes/2975.maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2975 lang=cpp
+ *
+ * [2975] Maximum Square Area by Removing Fences From a Field
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) {
+        
+    }
+};
+// @lc code=end
+
