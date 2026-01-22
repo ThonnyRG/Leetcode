c8efe43faa4764fc45fcd2f66f69473c39666cad
ThonnyRG <zs21017265@estudiantes.uv.mx>
2026-01-21 18:41:59 -0600

    Daily 21 jan 2026

---
+++ Leetcodes/3507.minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=3507 lang=cpp
+ *
+ * [3507] Minimum Pair Removal to Sort Array I
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  int minimumPairRemoval(vector<int>& nums) {
+    int ans = 0;
+
+    while (hasInversion(nums)) {
+      vector<int> pairSums;
+      for (const auto& [a, b] : views::pairwise(nums))
+        pairSums.push_back(a + b);
+      const int minPairSum = ranges::min(pairSums);
+      const int minPairIndex =
+          ranges::find(pairSums, minPairSum) - pairSums.begin();
+      nums[minPairIndex] = minPairSum;
+      nums.erase(nums.begin() + minPairIndex + 1);
+      ++ans;
+    }
+
+    return ans;
+  }
+
+ private:
+  bool hasInversion(const vector<int>& nums) {
+    for (const auto& [a, b] : views::pairwise(nums))
+      if (a > b)
+        return true;
+    return false;
+  }
+};
+// @lc code=end
+
