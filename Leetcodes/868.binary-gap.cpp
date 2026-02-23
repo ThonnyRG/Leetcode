913d43b09c6c95f96498618356bd2b2d3d00ddd4
ThonnyRG <zs21017265@estudiantes.uv.mx>
2026-02-22 18:13:22 -0600

    Daily 21 feb 2026

---
+++ Leetcodes/868.binary-gap.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=868 lang=cpp
+ *
+ * [868] Binary Gap
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int binaryGap(unsigned n) {
+        int d=0, p=32;
+        for(; n>0; n&=(n-1)){
+            int ctz=countr_zero(n);
+            d=max(d, ctz-p);
+            p=ctz;
+        }
+        return d;
+    }
+};
+// @lc code=end
+
