3eac099d904f82a6d2a1cd90370c98b930e3a468
ThonnyRG <zs21017265@estudiantes.uv.mx>
2026-03-11 18:21:59 -0600

    Daily 11 Mar  2026

---
+++ Leetcodes/3600.maximize-spanning-tree-stability-with-upgrades.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=3600 lang=cpp
+ *
+ * [3600] Maximize Spanning Tree Stability with Upgrades
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxStability(int n, vector<vector<int>>& edges, int k) {
+      int right = 0;
+for (auto& e : edges) right = max(right, e[2]);
+right *= 2;
+int left = -1;
+while (left < right) {
+    int mid = left + (right - left + 1) / 2;
+    if (check(mid))
+        left = mid;
+    else
+        right = mid - 1;
+}
+return left; 
+    }
+};
+// @lc code=end
+
