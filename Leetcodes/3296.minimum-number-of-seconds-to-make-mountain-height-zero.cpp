0b86b58be3e021f2f643cbb21f35e1fb2ab8f72a
ThonnyRG <zs21017265@estudiantes.uv.mx>
2026-03-15 11:41:20 -0600

    Daily 15 Mar  2026

---
+++ Leetcodes/3296.minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=3296 lang=cpp
+ *
+ * [3296] Minimum Number of Seconds to Make Mountain Height Zero
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        if(workerTimes.size()==1){
+            return workerTimes[0]* (long long)mountainHeight*(mountainHeight+1)/2;
+        }
+        priority_queue<pair<long long ,pair<long long ,long long >> , 
+         vector<pair<long long ,pair<long long ,long long >>> ,
+         greater<pair<long long ,pair<long long ,long long >>>>qu;
+
+         for(int i=0;i<workerTimes.size();i++){
+            qu.push({workerTimes[i],{workerTimes[i],1}});
+         }
+
+         int ans=0;
+
+         for(int i=0;i<mountainHeight;i++){
+            int time=qu.top().first;
+            int original=qu.top().second.first;
+            int height_reduced=qu.top().second.second;
+            qu.pop();
+
+            ans=time;
+            qu.push({time+(long long)original*(height_reduced+1) ,{original,height_reduced+1}});
+         }
+
+         return ans;
+
+    }
+};
+// @lc code=end
+
